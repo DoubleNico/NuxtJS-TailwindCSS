9f9b09f8237c93c4a93faa912b457049636fcb32
BuildTools <unconfigured@null.spigotmc.org>
2022-11-12 14:41:36 +0200

    Added more customization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,24 @@ export default defineNuxtConfig({
   colorMode: {
     fallback: 'dark', // fallback value if not system preference found
   },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Nuxt JS + Tailwind CSS',
+      meta: [
+        {
+          name: 'description',
+          content: 'A cool website with support for ESLINT and Prettier',
+        },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: 'https://raw.githubusercontent.com/nuxt/nuxt.js/master/.github/nuxt.png',
+        },
+      ],
+    },
+  },
 })
