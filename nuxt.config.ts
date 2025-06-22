270d81b114a87285ddfb9089c86b1eb19c2f2f10
DoubleNico <tomescuvlad123@gmail.com>
2025-06-22 21:43:02 +0300

    Updated to latest nuxt version

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/eslint',
+    '@nuxtjs/i18n',
+    '@nuxtjs/color-mode',
+  ],
+  i18n: {
+    locales: [
+      {
+        name: 'Română',
+        code: 'ro',
+        language: 'ro-RO',
+        file: 'ro-RO.json',
+      },
+      {
+        name: 'English',
+        code: 'en',
+        language: 'en-US',
+        file: 'en-US.json',
+      },
+    ],
+    langDir: './locales',
+    defaultLocale: 'en',
+    detectBrowserLanguage: {
+      useCookie: true,
+      alwaysRedirect: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
+    },
+    bundle: {
+      optimizeTranslationDirective: false,
+    },
+  },
+  css: ['~/assets/css/tailwind.css'],
+});
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './components/**/*.{js,vue,ts}',
+  './layouts/**/*.vue',
+  './pages/**/*.vue',
+  './plugins/**/*.{js,ts}',
+  './app.vue',
+  './error.vue',
+];
+export const darkMode = 'class';
+export const plugins = [];
